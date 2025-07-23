d8a3f347544e46b40a5e49b014873b749f2818ed
Md. Rifat Ahasan Pulock <pulok.github@gmail.com>
2025-07-23 23:36:49 +0600

    worked with post method.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+
 const cors = require('cors');
 
 
 app.use(cors());
+app.use(bodyParser.json());
 //routes or Middleware can be added here
 app.get('/',(req, res)=>{
     res.send('Hello World!');
@@ -22,6 +26,18 @@ app.get('/users',(req, res)=>{
     
     res.send(users)
 })
+
+//post method
+app.post('/love',(req, res)=>{
+    console.log(req.body);
+    res.send({
+        'name' : req.body.name,
+        'spouse' : req.body.spouse
+        
+
+    }).status(201);
+});
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
